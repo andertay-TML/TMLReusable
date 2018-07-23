340417eae99493f13236d936f8158c5b0593fffc
Ander Tay <andertay@anders-macbook-pro.local>
2018-07-23 22:55:43 +0800

    Initial Commit

---
+++ TMLReusable/TMLReusable.h
@@ -0,0 +1,19 @@
+//
+//  TMLReusable.h
+//  TMLReusable
+//
+//  Created by Ander Tay on 23/07/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMLReusable.
+FOUNDATION_EXPORT double TMLReusableVersionNumber;
+
+//! Project version string for TMLReusable.
+FOUNDATION_EXPORT const unsigned char TMLReusableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMLReusable/PublicHeader.h>
+
+
